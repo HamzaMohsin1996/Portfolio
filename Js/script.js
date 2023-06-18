f66b73a68118c1a8278ebdd06cf30180c3d91693
Hamzawarric <hamza95.hw@gmail.com>
2023-06-18 21:04:30 +0500

    testimonials section added

--- Js/script.js
+++ Js/script.js
@@ -3,6 +3,32 @@ $(document).ready(function(){
     $(this).toggleClass("active");
     $("#header-wrapper").toggleClass("active");
   });
+  $('.works-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    margin:5,
+    centerMode:true,
+  });
+  $('.testimonials-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    centerMode:true,
+    dots:true,
+    responsive: [
+      {
+          breakpoint: 1600,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll:1,
+          }
+      },
+    
+    ]
+  });
 });
 window.addEventListener("scroll", function() {
   var header = document.getElementById("header-wrapper");
